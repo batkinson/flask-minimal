87a5fdf6b8832f9e69e1ee27ce21939c6375d522
Brent Atkinson <brent.atkinson@gmail.com>
2014-08-25 15:33:15 -0400

    Added simple session usage.

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
+
 
 # Our application
 app = Flask(__name__)
 app.secret_key = 'this doesn\'t really matter for this example'
 
+
 # Our route handlers
 @app.route('/')
 def index():
+   if 'num' in session:
+      return render_template('home.html', num=session['num'])
    return render_template('home.html')
+
+
+@app.route('/addone')
+def addone():
+   if not 'num' in session:
+      session['num'] = 1
+   else:
+      session['num'] += 1
+   return redirect(url_for('index'))
+
+
+@app.route('/clear')
+def clear():
+   session.pop('num')
+   return redirect(url_for('index'))
+
 
 # Run the application as a stand-alone
 if __name__ == '__main__':
